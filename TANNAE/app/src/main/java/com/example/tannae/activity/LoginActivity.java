5c40e2ad89e0a05fe39a3fddcfd838a216f5095a
JaeWon Choi <unsyg.choi@gmail.com>
2022-04-04 14:04:19 +0900

    [Update] LoginActivity
    - Init Class

--- TANNAE/app/src/main/java/com/example/tannae/activity/LoginActivity.java
+++ TANNAE/app/src/main/java/com/example/tannae/activity/LoginActivity.java
@@ -11,19 +11,6 @@ import com.example.tannae.R;
 import com.example.tannae.entity.UserEntity;
 
 public class LoginActivity extends AppCompatActivity {
-    // Views
-    private EditText textId, textPw;
-    private Button btnLogin;
-    private TextView tvFind, tvSignUp;
-
-    // Data
-    public UserEntity me;
-
-    // < Methods >
-    private void onClickFindAccount(View view) {}
-    private void onClickSingUp(View view) {}
-    private void onClickLogin(View view) {}
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
