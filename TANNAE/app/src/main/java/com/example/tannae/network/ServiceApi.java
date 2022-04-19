e25ce48b4c2da5e023f550161313c9f35ab34f6f
JaeWon Choi <unsyg.choi@gmail.com>
2022-04-19 19:07:58 +0900

    [Update] Activity/Network JW04191907
    - LoginActivity.java
    - ServiceApi.java

--- TANNAE/app/src/main/java/com/example/tannae/network/ServiceApi.java
+++ TANNAE/app/src/main/java/com/example/tannae/network/ServiceApi.java
@@ -3,9 +3,12 @@ package com.example.tannae.network;
 import org.json.JSONObject;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface ServiceApi {
-    @POST("/user/login")
-    Call<String> login(@Body JSONObject json);
+    /* Account */
+    @GET("/account/login")                  // Login
+    Call<String> login(@Query("id") String id, @Query("pw") String pw, @Query("ip") String ip);
 }
