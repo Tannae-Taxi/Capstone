691433c05470cb82ec505ddf9aacd11ab42ad0f4
JaeWon Choi <unsyg.choi@gmail.com>
2022-04-21 15:05:05 +0900

    [Update] ServerApi.java JW04211504
    - Account 관련 GET/POST 구현

--- TANNAE/app/src/main/java/com/example/tannae/network/ServiceApi.java
+++ TANNAE/app/src/main/java/com/example/tannae/network/ServiceApi.java
@@ -11,4 +11,16 @@ public interface ServiceApi {
     /* Account */
     @GET("/account/login")                  // Login
     Call<String> login(@Query("id") String id, @Query("pw") String pw);
+    @GET("account/checkID")                 // Check ID Duplication
+    Call<String> checkID(@Query("id") String id);
+    @POST("/account/signup")                // Sign Up
+    Call<String> signup(@Body JSONObject user);
+    @GET("/account/findAccount")
+    Call<String> findAccount(@Query("uname") String uname, @Query("rrn") String rrn, @Query("phone") String phone, @Query("email") String email);
+
+    /* User */
+
+    /* Driver */
+
+    /* Passenger */
 }
