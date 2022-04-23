7d01059b1b05055797b35bc327038ffa902bfaf5
Ubuntu <ubuntu@ip-172-31-36-200.ap-northeast-2.compute.internal>
2022-04-23 14:07:04 +0000

    [Update] server.js JW04232307

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ let bodyParser = require('body-parser');
 
 // < Uses >
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // < MySQL Connection >
 connection = mysql.createConnection({
@@ -27,59 +27,91 @@ app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
 
-// << Reqeust & Response >>
+// << Reqeust & Response >>6
 // < Account >
 // Login
 app.get('/account/login', (req, res) => {
-    let id = req.query.id;
-    let pw = req.query.pw;
     let sql = 'select * from User where binary id = ?';
-    connection.query(sql, id, (err, result) => {
-        let jsErr = {"error": "false"};
-
-        if(err) {
-            console.log(err);
-            jsErr.error = "MySQL Error";
+    connection.query(sql, req.query.id, (err, result) => {
+        let resType = { "resType": "OK" };
+        if (err) {
+            console.log(err.code);
+            resType.resType = "Error";
+            res.json(JSON.stringify([resType]));
         } else {
-            if(result.length === 0) {
-                jsErr.error = "등록된 사용자가 아닙니다.";
+            if (result.length === 0) {
                 console.log('/account/login : Not a user');
-            } else if(pw !== result[0].pw.toString('utf-8')) {
-                jsErr.error = "비밀번호가 잘못되었습니다.";
+                resType.resType = "등록된 사용자가 아닙니다.";
+            } else if (req.query.pw !== result[0].pw.toString('utf-8')) {
                 console.log('/account/login : Password mismatch');
-            } else {
+                resType.resType = "비밀번호가 잘못되었습니다.";
+            } else
                 console.log('/account/login : Login success');
-            }
+            result.unshift(resType);
+            res.json(JSON.stringify(result));
         }
-        result.unshift(jsErr);
-        res.json(JSON.stringify(result));
     });
 });
 
+// Check ID
 app.get('/account/checkID', (req, res) => {
-    console.log('CheckID');
-    let id = req.query.id;
-    console.log(id);
+    let sql = 'select * from User where binary id = ?';
+    connection.query(sql, req.query.id, (err, result) => {
+        let resType = {"resType": "OK"};
+        if(err) {
+            console.log(err.code);
+            resType.resType = "Error";
+            res.json(JSON.stringify([resType]));
+        } else {
+            if(result.length !== 0) {
+                console.log('/account/checkID : Used ID');
+                resType.resType = "사용할 수 없는 ID 입니다.";
+            } else
+                console.log('/account/checkID : ID permitted');
+        }
+    });
 });
 
+
+// Sign Up
 app.post('/account/signup', (req, res) => {
     let jsonReq = req.body.nameValuePairs;
     let sql = 'insert User values(?, ?, ?, ?, ?, ?, ?, ?, false, 0, 4.5)'
     let usn = fun.usnGenerator();
     connection.query(sql, [usn, jsonReq.id, jsonReq.pw, jsonReq.uname, jsonReq.rrn, jsonReq.sex, jsonReq.phone, jsonReq.email, false, 0, 4.5], (err, result) => {
-        let jsErr = {"error": "false"};
-        if(err) {
+        let resType = { "resType": "OK" };
+        if (err) {
             console.log(err);
-            jsErr.error = "MySQL Error";
-        } else {
-            jsErr.error = "Sign up complete";
-            console.log('/account/signup : Sign Up complet');
-        }
-        result.unshift(jsErr);
+            resType.resType = "Error";
+        } else
+            console.log('/account/signup : Sign Up complete');
+        result.unshift(resType);
         res.json(JSON.stringify(result));
     });
 });
 
+// Find Account
 app.get('/account/findAccount', (req, res) => {
+    let uname = req.query.uname;
+    let rrn = req.query.rrn;
+    let phone = req.query.phone;
+    let email = req.query.email;
+    let sql = 'select * from User where uname = ?';
 
+    connection.query(sql, uname, (err, result) => {
+        let resType = { "resType": "OK" };
+        if (err) {
+            console.log(err);
+            resType.resType = "Error";
+        } else if (result.length === 0) {
+            console.log('/account/findAccount : Not a user');
+            resType.resType = "등록된 사용자가 아닙니다.";
+        } else if (result[0].rrn !== rrn || result[0].phone !== phone || result[0].emial !== email) {
+            console.log('/account/findAccount : Wrong rrn');
+            resType.resType = "잘못된 사용자 정보입니다."
+        } else
+            console.log('/account/findAccount : Found user');
+        result.unshift(resType);
+        res.json(JSON.stringify(result));        
+    });
 });
