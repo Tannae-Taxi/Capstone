68a003f16a5857b92da65272d88896bfaaadac24
schan22 <101787631+schan22@users.noreply.github.com>
2022-04-25 05:05:03 +0900

    [Update] 로그인 관련 SC04250501
    - activity_find.xml 수정
      - 성별 부분 라디오 그룹 추가
      - birth 대신 rrn으로 수정
    
    - FindActivity.java 수정
      - setViews메소드 추가
      - 이벤트리스너 등록
    
    - activity_sign_up.xml 수정
      - 텍스트뷰 부분에 아이디 규칙 정의
    - SignUpActivity.java 수정
      - 아이디와 비밀번호 체크하는 코드 작성

--- TANNAE/app/src/main/java/com/example/tannae/activity/FindActivity.java
+++ TANNAE/app/src/main/java/com/example/tannae/activity/FindActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.widget.NestedScrollView;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.tannae.R;
@@ -14,28 +17,52 @@ import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class FindActivity extends AppCompatActivity {
+    private TextView tvPersonal, tvMyId, tvMyPw;
+    private EditText etName, etRRN, etEmail, etPhoneNumber, etPinNumber;
+    private RadioGroup rgSex;
+    private Button btnFindAccount, btnCertificate;
+
+    private boolean checkedSex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find);
-
-        AppBarLayout appBarLayout3 = findViewById(R.id.appBarLayout3);
-        MaterialToolbar topAppBar = findViewById(R.id.topAppBar);
-        NestedScrollView nestedScrollView = findViewById(R.id.nestedScrollView);
-
-        TextView tvPersonal = findViewById(R.id.tv_personal);
-        EditText etName = findViewById(R.id.et_name);
-        EditText etBirth = findViewById(R.id.et_birth);
-        RadioButton man = findViewById(R.id.man);
-        RadioButton woman = findViewById(R.id.woman);
-        EditText etEmail = findViewById(R.id.et_email);
-        EditText etPhoneNumber = findViewById(R.id.et_phone);
-        Button btnFindAccount= findViewById(R.id.btn_findaccount);
-        EditText etPinNumber = findViewById(R.id.et_pinnumber);
-        Button btnCertificate = findViewById(R.id.btn_Certificate);
-        TextView tvMyId = findViewById(R.id.tv_myid);
-        TextView tvMyPw = findViewById(R.id.tv_mypw);
-
+        setViews();
+        setEventListeners();
     }
+    private void setViews(){
+        tvPersonal = findViewById(R.id.tv_personal);
+        etName = findViewById(R.id.et_name);
+        etRRN = findViewById(R.id.et_rrn);
+        rgSex = findViewById(R.id.rg_sex);
+        etEmail = findViewById(R.id.et_email);
+        etPhoneNumber = findViewById(R.id.et_phone);
+        btnFindAccount= findViewById(R.id.btn_findaccount);
+        etPinNumber = findViewById(R.id.et_pinnumber);
+        btnCertificate = findViewById(R.id.btn_Certificate);
+        tvMyId = findViewById(R.id.tv_myid);
+        tvMyPw = findViewById(R.id.tv_mypw);
+    }
+
+    private void setEventListeners(){
+        rgSex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                checkedSex = (checkedId == R.id.rb_man) ? true : false;
+            }
+        });
+        etRRN.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                String rrn = etRRN.getText().toString();
+                if(rrn.length() == 7 && rrn.charAt(rrn.length() - 1) != '-' ) {
+                    etRRN.setText(new StringBuffer(rrn).insert(6, '-').toString());
+                    etRRN.setSelection(rrn.length() + 1);
+                }
+                return false;
+            }
+        });
+    }
+
 }
