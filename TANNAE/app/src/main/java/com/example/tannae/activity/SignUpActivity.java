4f26680f4698832ab450fef78a479481fcc39f1f
JaeWon Choi <unsyg.choi@gmail.com>
2022-04-24 12:10:54 +0900

    [Update] SignUpActivity.java JW04241210
    - Add three flags for siging up

--- TANNAE/app/src/main/java/com/example/tannae/activity/SignUpActivity.java
+++ TANNAE/app/src/main/java/com/example/tannae/activity/SignUpActivity.java
@@ -1,6 +1,8 @@
 package com.example.tannae.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.tannae.R;
 import com.example.tannae.network.Network;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -21,7 +24,7 @@ import retrofit2.Response;
 public class SignUpActivity extends AppCompatActivity {
     private Button btnCheckID, btnSignUp;
     private EditText etID;
-    private boolean availableID;
+    private boolean availableID = false, availablePW = false, checkedID = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +41,13 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private void setEventListeners() {
+        etID.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                checkedID = false;
+                return false;
+            }
+        });
         btnCheckID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,12 +58,26 @@ public class SignUpActivity extends AppCompatActivity {
                 Network.service.checkID(etID.getText().toString()).enqueue(new Callback<String>() {
                     @Override
                     public void onResponse(Call<String> call, Response<String> response) {
-
+                        try {
+                            JSONArray resArr = new JSONArray(response.body());
+                            JSONObject resObj = resArr.getJSONObject(0);
+                            String resType = resObj.getString("resType");
+                            if (resType.equals("OK")) {
+                                checkedID = true;
+                                Toast.makeText(getApplicationContext(), "사용 가능한 ID 입니다.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                checkedID = false;
+                                Toast.makeText(getApplicationContext(), "이미 사용 중인 ID 입니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
 
                     @Override
                     public void onFailure(Call<String> call, Throwable t) {
-
+                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                        Log.e("Error", t.getMessage());
                     }
                 });
             }
