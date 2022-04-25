00a42ae39fe4996ea7857e57c4877fe9b55f456d
schan22 <101787631+schan22@users.noreply.github.com>
2022-04-26 00:23:47 +0900

    [Update] 뷰 등록 및 메니페스트.xml 수정 SC04260022
    - 모든 클래스에 뷰, 이벤트메소드 등록
    - AndroidManifest.xml에 클래스들 등록(intent 용)

--- TANNAE/app/src/main/java/com/example/tannae/activity/MainActivity.java
+++ TANNAE/app/src/main/java/com/example/tannae/activity/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.tannae.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Switch;
 
 import com.example.tannae.R;
 import com.google.android.material.appbar.MaterialToolbar;
@@ -11,16 +14,42 @@ import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private Switch sDrive;
+    private ImageButton ibMap;
+    private FloatingActionButton reqBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setViews();
+        setEventListeners();
+    }
 
-        MaterialToolbar topAppBar = findViewById(R.id.topAppBar);
-        BottomAppBar bottomAppBar = findViewById(R.id.bottomAppBar);
-        FloatingActionButton reqButton = findViewById(R.id.req_button);
+    private void setViews() {
+        sDrive = findViewById(R.id.s_drive_main);
+        ibMap = findViewById(R.id.ib_map_main);
+        reqBtn = findViewById(R.id.req_button);
+    }
 
-        ImageButton ibMap = findViewById(R.id.ib_map_main);
+    private void setEventListeners() {
+        sDrive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ibMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        reqBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- TANNAE/app/src/main/java/com/example/tannae/activity/PointActivity.java
+++ TANNAE/app/src/main/java/com/example/tannae/activity/PointActivity.java
@@ -1,17 +1,58 @@
 package com.example.tannae.activity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tannae.R;
 
 public class PointActivity extends AppCompatActivity {
+    private TextView tvPoint;
+    private EditText etCharge;
+    private Button btnCharge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_point);
+        setViews();
+        setEventListeners();
+    }
 
+    private void setViews() {
+        tvPoint = findViewById(R.id.tv_point_point);
+        etCharge = findViewById(R.id.et_charge_point);
+        btnCharge = findViewById(R.id.btn_charge_point);
+    }
+
+    private void setEventListeners() {
+        etCharge.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        btnCharge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
