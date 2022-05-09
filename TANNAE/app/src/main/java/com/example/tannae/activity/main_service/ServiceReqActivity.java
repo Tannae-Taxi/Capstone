12c3edb3ffc5bd5f13ec33c139c1732e98203714
qhfhdls <79788971+qhfhdls@users.noreply.github.com>
2022-05-10 03:37:21 +0900

    [Update] ServiceReqActivity.java activity_servicereq.xml DH05100324
    servicereq Map 만지기 완료
    ( 정보 저장 미 완료 - 궁금한 사항들)
    ( JSON 미 완료 )

--- TANNAE/app/src/main/java/com/example/tannae/activity/main_service/ServiceReqActivity.java
+++ TANNAE/app/src/main/java/com/example/tannae/activity/main_service/ServiceReqActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,17 +13,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.tannae.R;
 import com.example.tannae.network.Network;
 
+import net.daum.android.map.MapViewEventListener;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapReverseGeoCoder;
 import net.daum.mf.map.api.MapView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+//class ReqLocation {
+//    double orgin_X = 0;
+//    double orgin_Y = 0;
+//    double destination_X = 0;
+//    double destination_Y = 0;
+//
+//    String origin_Location = "";
+//    String destination_Location = "";
+//}
+
 // << ServiceReq Activity >>
-public class ServiceReqActivity extends AppCompatActivity {
+public class ServiceReqActivity extends AppCompatActivity implements MapView.MapViewEventListener, MapView.POIItemEventListener, MapReverseGeoCoder.ReverseGeoCodingResultListener{
+    private RadioGroup rglocation;
     private TextView tvOrigin, tvDestination;
     private Button btnServiceReq, btnBack;
+    private boolean locationType = true;
     private MapView mapView;
     private ViewGroup mapViewContainer;
+    private MapPoint mapPoint;
+    private MapPOIItem marker;
 
     // < onCreate >
     @Override
@@ -33,6 +52,7 @@ public class ServiceReqActivity extends AppCompatActivity {
         // Setting
         setViews();
         setEventListeners();
+
     }
 
     @Override
@@ -40,6 +60,17 @@ public class ServiceReqActivity extends AppCompatActivity {
         super.onResume();
         mapView = new MapView(this);
         mapViewContainer = (ViewGroup) findViewById(R.id.map_view_servicereq);
+
+        mapView.setMapViewEventListener(this);
+        mapView.setPOIItemEventListener(this);
+
+        marker = new MapPOIItem();
+        marker.setItemName("위치");
+        marker.setTag(0);
+        marker.setMapPoint(mapView.getMapCenterPoint());
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+
         mapViewContainer.addView(mapView);
     }
 
@@ -51,6 +82,7 @@ public class ServiceReqActivity extends AppCompatActivity {
 
     // < Register views >
     private void setViews() {
+        rglocation = findViewById(R.id.rg_location_servicereq);
         btnServiceReq = findViewById(R.id.btn_request_servicereq);
         btnBack = findViewById(R.id.btn_back_servicereq);
         tvOrigin = findViewById(R.id.tv_origin_servicereq);
@@ -66,6 +98,15 @@ public class ServiceReqActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        rglocation.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                locationType = (checkedId == R.id.rb_origin_servicereq) ? true : false;
+            }
+        });
+
+
         // Request Service [SOCKET]
         btnServiceReq.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,5 +145,123 @@ public class ServiceReqActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
+    }
+
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+
+        mapPoint = mapView.getMapCenterPoint();
+        MapReverseGeoCoder mapGeoCoder = new MapReverseGeoCoder("be32c53145962ae88db090324e2223b0",
+                mapPoint, this, ServiceReqActivity.this);
+        mapGeoCoder.startFindingAddress();
+
+//        if(locationType == true){
+//            ReqLocation.orgin_Y = mapPoint.getMapPointGeoCoord().longitude;
+//            ReqLocation.orgin_X = mapPoint.getMapPointGeoCoord().latitude;
+//        }
+//        else{
+//            ReqLocation.destination_Y = mapPoint.getMapPointGeoCoord().longitude;
+//            ReqLocation.destination_X = mapPoint.getMapPointGeoCoord().latitude;
+//        }
+        marker.setMapPoint(mapPoint);
+        mapView.addPOIItem(marker);
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+        // this
+        mapPoint = mapView.getMapCenterPoint();
+        MapReverseGeoCoder mapGeoCoder = new MapReverseGeoCoder("be32c53145962ae88db090324e2223b0",
+                mapPoint, this, ServiceReqActivity.this);
+        mapGeoCoder.startFindingAddress();
+
+//        if(locationType == true){
+//            ReqLocation.orgin_Y = mapPoint.getMapPointGeoCoord().longitude;
+//            ReqLocation.orgin_X = mapPoint.getMapPointGeoCoord().latitude;
+//        }
+//        else{
+//            ReqLocation.destination_Y = mapPoint.getMapPointGeoCoord().longitude;
+//            ReqLocation.destination_X = mapPoint.getMapPointGeoCoord().latitude;
+//        }
+
+        marker.setMapPoint(mapPoint);
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+
+    }
+
+    @Override
+    public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onReverseGeoCoderFoundAddress(MapReverseGeoCoder mapReverseGeoCoder, String s) {
+        if(locationType == true){
+            tvOrigin.setText(s);
+//            ReqLocation.origin_Location = s;
+        }
+        else{
+            tvDestination.setText(s);
+//            ReqLocation.destination_Location = s;
+        }
+
+    }
+
+    @Override
+    public void onReverseGeoCoderFailedToFindAddress(MapReverseGeoCoder mapReverseGeoCoder) {
+        if(locationType == true)
+            tvOrigin.setText("올바른 지역이 아닙니다.");
+        else
+            tvDestination.setText("올바른 지역이 아닙니다.");
+
     }
 }
