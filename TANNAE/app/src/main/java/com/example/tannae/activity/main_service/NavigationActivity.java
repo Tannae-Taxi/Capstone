8c4f37b7ec3b1b82f0b7c98828885a62b104115d
JaeWon Choi <unsyg.choi@gmail.com>
2022-05-11 14:04:42 +0900

    [Update] NavigationActivity.java JW05111404
    - flag에 따른 Toast message 처리

--- TANNAE/app/src/main/java/com/example/tannae/activity/main_service/NavigationActivity.java
+++ TANNAE/app/src/main/java/com/example/tannae/activity/main_service/NavigationActivity.java
@@ -17,7 +17,6 @@ import com.example.tannae.user.User;
 
 import net.daum.mf.map.api.MapView;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 // << Navigation Activity >>
@@ -46,6 +45,7 @@ public class NavigationActivity extends AppCompatActivity {
         }
     }
 
+    // < onResume >
     @Override
     protected void onResume() {
         super.onResume();
@@ -54,6 +54,7 @@ public class NavigationActivity extends AppCompatActivity {
         mapViewContainer.addView(mapView);
     }
 
+    // < onPause >
     @Override
     protected void onPause() {
         super.onPause();
@@ -61,17 +62,44 @@ public class NavigationActivity extends AppCompatActivity {
     }
 
     // < Set Socket.io >
-    private void setNetworks() { /////////////////////////////////////// 탑승자 유저가 배차가 성공하였을 경우 state 값을 1로 변경하기
+    private void setNetworks() {
+        // Response service
         Network.socket.on("responseService", args -> {
+            // args[0] : flag / args[1] : path / args[2] : usn
             runOnUiThread(() -> {
+                // Flag : -1 (Server Error) / 0 (No Vehicle) / 1 (Share vehicle) / 2 (Non-share vehicle to share user) / 3 (Non-share vehicle to non-share user)
+                // 4 (Passenger boarding) / 5 (Passenger get off)
                 int flag = (int) args[0];
-                String message = flag == -1 ? "배차 오류가 발생하였습니다.\n고객센터에 문의하세요." : (flag == 0 ? "이용 가능한 차량이 없습니다." :
-                        (flag == 1 ? "동승 가능한 차량이 배차되었습니다." : (flag == 2 ? "동승 가능한 차량이 없습니다.\n일반 차량이 배차되었습니다." : "일반 차량이 배차되었습니다.")));
-                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                // Set message
+                String message;
+                switch (flag) {
+                    case 0 : message = "이용 가능한 차량이 없습니다."; break;
+                    case 1 : message = "동승 가능한 차량이 배차되었습니다."; break;
+                    case 2 : message = "동승 가능한 차량이 없습니다.\n일반 차량이 배차되었습니다."; break;
+                    case 3 : message = "일반 차량이 배차되었습니다."; break;
+                    case 4 : message = "탑승자가 승차하였습니다.\n경로를 수정합니다."; break;
+                    case 5 : message = "탑승자가 하차하였습니다.\n경로를 수정합니다."; break;
+                    default: message = "배차 오류가 발생하였습니다.\n고객센터에 문의하세요."; break;
+                }
 
-                if (flag == 1 || flag == 2 || flag == 3) {
+                // Show message
+                if (flag == 1 || flag == 2 || flag == 3)
+                    if (((String)args[2]).equals(User.sp.getString("usn", "")))
+                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(getApplicationContext(), "추가 인원이 배차 되었습니다.\n경로를 수정합니다.", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+                ////////////////////////////////////// 밑에는 flag에 따른 처리 아직 안함
+                // Event handle by flag number
+                if (!(flag == -1 || flag == 0)) {
                     JSONObject path = (JSONObject) args[1];
                     ////////////////////////////////////////////////////////////// path 정보를 바탕으로 navigation 화면 수정
+                    /////////////////////////////////////// state 값을 1로 변경하기 (다만, 운전자의 state는 미변경)
+                    ///////////////////////// flag : int args[0] / path : JSONObject args[1] / usn : String args[2]
+                    ///////////////////////// usn은 현재 경유지 탑승자의 usn : 내부 db의 usn과 일치하며 flag가 5이면 내부 db user의 state를 false(0)로 변경
+                    // flag -1 0은 flag이외의 arg가 없으며 1 2 3은 path arg가 추가되고 4 5는 usn이 추가
                 } else {
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -138,7 +166,7 @@ public class NavigationActivity extends AppCompatActivity {
         /////////////////////////////// Navigation 화면에 있는데 state 가 0인 사람은 운전자이고 1인 사람은 탑승자임을 알 수 있슴.
         /////////////////////////////// 운전자는 안전상의 이유로 운행 중일 때는 Navigation 종료 불가
         /////////////////////////////// 탑승자는 Main 화면으로 복귀
-        if(User.sp.getInt("state", 0) == 0 && User.sp.getInt("drive",0) == 1) // 내비게이션 화면에 있는데 운전자(state값이 0)이며 운행중(drive 값이 1)상태인경우에는
+        if (User.sp.getInt("state", 0) == 0 && User.sp.getInt("drive", 0) == 1) // 내비게이션 화면에 있는데 운전자(state값이 0)이며 운행중(drive 값이 1)상태인경우에는
             Toast.makeText(getApplicationContext(), "운행중에는 내비게이션을 종료할 수 없습니다.", Toast.LENGTH_SHORT).show(); // 내비 종료 불가
         else {
             /////////////////////////// Main 화면으로 복귀
