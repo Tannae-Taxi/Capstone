13fc62d89892cabeea71e40d869250f283aa900a
JaeWon Choi <unsyg.choi@gmail.com>
2022-05-13 22:10:33 +0900

    [Update] Navigation JW05132210
    - Navigation 처리

--- TANNAE/app/src/main/java/com/example/tannae/activity/main_service/NavigationActivity.java
+++ TANNAE/app/src/main/java/com/example/tannae/activity/main_service/NavigationActivity.java
@@ -1,20 +1,21 @@
 package com.example.tannae.activity.main_service;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tannae.R;
 import com.example.tannae.network.Network;
-import com.example.tannae.sub.Path;
-import com.example.tannae.sub.User;
+import com.example.tannae.sub.InnerDB;
 
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
@@ -26,6 +27,7 @@ import org.json.JSONObject;
 public class NavigationActivity extends AppCompatActivity {
     private Button btnEndService, btnPass;
     private Switch switchDrive;
+    private TextView tvNext;
     private MapView mapView;
     private ViewGroup mapViewContainer;
     private boolean type;
@@ -42,11 +44,19 @@ public class NavigationActivity extends AppCompatActivity {
         setNetworks();
         // If main -> navigation ? true : false
         type = getIntent().getBooleanExtra("type", false);
+        btnPass.setBackgroundColor(Color.parseColor("#BDBDBD"));
+        btnEndService.setBackgroundColor(Color.parseColor("#BDBDBD"));
         if (!type) {
             btnPass.setVisibility(View.INVISIBLE);
             btnEndService.setVisibility(View.INVISIBLE);
             switchDrive.setVisibility(View.INVISIBLE);
-            //////////////////////////// 내부 state 가 1이면 (차를 타고 있는데 Navigation 화면에서 나갔다가 다시 들어온 경우이면) 내부에 저장된 path 를 바탕으로 MapView 다시 그리기
+            try {
+                JSONObject path = new JSONObject(InnerDB.sp.getString("path", ""));
+                //////////////////////////////////////////////////////////// path(JSON)을 Mapview 띄우기
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
@@ -78,9 +88,7 @@ public class NavigationActivity extends AppCompatActivity {
                 int flag = (int) args[0];
                 JSONObject path = (JSONObject) args[1];
                 String usnOut = (String) args[2];
-                String usnIn = User.sp.getString("usn", "");
-
-                // Update inner DB
+                String usnIn = InnerDB.sp.getString("usn", "");
 
                 // Toast
                 String message;
@@ -94,41 +102,57 @@ public class NavigationActivity extends AppCompatActivity {
                     case 5 : message = usnOut.equals(usnOut) ? "목적지에 도착하였습니다.\n하차해 주시기 바랍니다." : "탑승자가 하차하였습니다.\n경로를 수정합니다."; break;
                     default: message = "배차 오류가 발생하였습니다.\n고객센터에 문의하세요."; break;
                 }
+
                 Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
 
-                // Event handle by flag number
-                if (!(flag == -1 || flag == 0)) {
-                    //////////////////////////////////////// path 정보를 문자열로 변환하여 내부 DB의 path 속성에 저장
-                    // => path정보를 저장할 별도의 내부 Path DB를 만들어야 하는지 (내부 User DB 말고) => 일단 이대로 코드 추가함: sc가 추가
-                    try {
-                        Path.setUserOutTOIn(path);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                try {
+                    // Event handle by flag number
+                    if (!(flag == -1 || flag == 0)) {
+                        if (flag == 1 || flag == 2 || flag == 3) {
+                            InnerDB.editor.putInt("state", usnOut.equals(usnIn) ? 1 : InnerDB.sp.getInt("state", 0)).apply();
+                            InnerDB.editor.putString("path", path.toString()).apply();
+                            btnPass.setEnabled(true);
+                            btnPass.setBackgroundColor(Color.parseColor("#FF127CEA"));
+                            btnPass.setText("경유");
+                            tvNext.setText("NEXT : " + path.getJSONArray("waypoints").getJSONObject(0).getString("name"));
+                            ///////////////////////// 지도 띄우기 path.origin / path.waypoints / path.destination
+                        } else if (flag == 4) {
+                            InnerDB.editor.putString("path", path.toString()).apply();
+                            tvNext.setText("NEXT : " + path.getJSONArray("waypoints").getJSONObject(0).getString("name"));
+                            //////////////////////// 지도 띄우기
+                        } else if (flag == 5) {
+                            if (usnOut.equals(usnIn)) {
+                                InnerDB.editor.putInt("state", 0).apply();
+                                InnerDB.editor.putString("path", null).apply();
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                            } else if (path != null){
+                                InnerDB.editor.putString("path", path.toString()).apply();
+                                tvNext.setText("NEXT : " + path.getJSONArray("waypoints").getJSONObject(0).getString("name"));
+                                if (path.getJSONArray("waypoints").length() == 0)
+                                    btnPass.setText("도착");
+                                ///////////////////////// 지도 띄우기
 
-                    if (flag == 1 || flag == 2 || flag == 3) {
-                        //////////////////////// 외부 usn(usnOut) 과 내부 usn(usnIn) 을 비교하여 일치하면 내부 state 을 1로 설정
-                        if(usnOut == usnIn)
-                            User.sp.edit().putInt("state", 1).apply();
-                    } else if (flag == 5){
-                        //////////////////////// 외부 usn(usnOut) 과 내부 usn(usnIn) 을 비교하여 일치하면 내부 state 을 0으로 설정
-                        if(usnOut == usnIn) {
-                            User.sp.edit().putInt("state", 0).apply();
-                            //////////////////////// 외부 usn(usnOut) 과 내부 usn(usnIn) 을 비교하여 일치하면 내부 path 정보를 null 로 변경
-                            // => path정보를 저장할 별도의 내부 Path DB를 만들어야 하는지 (내부 User DB 말고), 바로 위의 if 문장(if(usnOut == usnIn))과 같은 조건이므로 같은 블럭에 넣으면 되는지 => 일단 이대로 코드 추가함: sc가 추가
-                            Path.setNull();
-                            //////////////////////// 외부 usn(usnOut) 과 내부 usn(usnIn) 이 일치하고 현재 화면이 navigation 이면 Main 화면으로 복귀
-                            // 이 경우에도 바로 위의 if 문장(if(usnOut == usnIn))과 같은 조건이므로 같은 블럭에 넣으면 되는지, 그리고 navigationActivity에서 호출되는 메소드이므로 현재 화면이 항상 내비인 것인지 => 일단 이대로 코드 추가함: sc가 추가
-                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                            startActivity(intent);
+                            } else {
+                                InnerDB.editor.putString("path", null).apply();
+                                tvNext.setText("요금을 정산해주세요.");
+                                btnPass.setEnabled(false);
+                                btnPass.setBackgroundColor(Color.parseColor("#BDBDBD"));
+                                btnPass.setText("경유");
+                                btnEndService.setEnabled(true);
+                                btnEndService.setBackgroundColor(Color.parseColor("#FF127CEA"));
+                                switchDrive.setChecked(false);
+                                //////////////////////// 지도 지우기
+                            }
                         }
+                    } else {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
                     }
-                    //////////////////////// DH : 가져온 path 정보를 바탕으로 navigation mapview 표시
-                } else {
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent);
+                } catch (JSONException e) {
+
                 }
             });
         });
@@ -147,6 +171,7 @@ public class NavigationActivity extends AppCompatActivity {
         btnPass = findViewById(R.id.btn_pass_navigation);
         btnEndService = findViewById(R.id.btn_end_service_navigation);
         switchDrive = findViewById(R.id.switch_drive_state_navigation);
+        tvNext = findViewById(R.id.tv_waypoints_navigation);
     }
 
     // < Register event listeners >
@@ -157,7 +182,7 @@ public class NavigationActivity extends AppCompatActivity {
             public void onClick(View v) {
                 try {
                     JSONObject driver = new JSONObject();
-                    User.setUserInTOOut(driver);
+                    InnerDB.setUserInTOOut(driver);
                     Network.socket.emit("passWaypoint", driver);
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -170,7 +195,7 @@ public class NavigationActivity extends AppCompatActivity {
             public void onClick(View v) {
                 try {
                     JSONObject driver = new JSONObject();
-                    User.setUserInTOOut(driver);
+                    InnerDB.setUserInTOOut(driver);
                     Network.socket.emit("serviceEnd", driver);
                     Toast.makeText(getApplicationContext(), "운행이 종료되었습니다.", Toast.LENGTH_SHORT).show();
                     /////////////////////////////////////////// MapView clear
@@ -183,13 +208,17 @@ public class NavigationActivity extends AppCompatActivity {
         switchDrive.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                JSONObject driver = new JSONObject();
-                try {
-                    User.setUserInTOOut(driver);
-                    driver.put("service", isChecked);
-                    Network.socket.emit(isChecked ? "serviceOn" : "serviceOff", driver);
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                if (btnEndService.isEnabled())
+                    Toast.makeText(getApplicationContext(), "요금을 정산해주세요.", Toast.LENGTH_SHORT).show();
+                else {
+                    JSONObject driver = new JSONObject();
+                    try {
+                        InnerDB.setUserInTOOut(driver);
+                        driver.put("service", isChecked);
+                        Network.socket.emit(isChecked ? "serviceOn" : "serviceOff", driver);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
@@ -197,12 +226,16 @@ public class NavigationActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (type)
-            Toast.makeText(getApplicationContext(), "운행중에는 내비게이션을 종료할 수 없습니다.", Toast.LENGTH_SHORT).show(); // 내비 종료 불가
-        else {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
+        if (switchDrive.isChecked()) {
+            Toast.makeText(getApplicationContext(), "서비스 제공 중에는 종료할 수 없습니다.", Toast.LENGTH_SHORT).show();
+        } else {
+            if (type && !InnerDB.sp.getString("path", "NULL").equals("NULL")) {
+                Toast.makeText(getApplicationContext(), "운행중에는 내비게이션을 종료할 수 없습니다.", Toast.LENGTH_SHORT).show();
+            } else {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
         }
     }
 }
