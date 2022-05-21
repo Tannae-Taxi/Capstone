a06a3f832ae1d33648ad7c71443eb0cdb800bf94
JaeWon Choi <unsyg.choi@gmail.com>
2022-05-21 14:08:01 +0900

    [Update} History|community_listview|list_listview JW05211407
    - 이용 기록 출력을 위한 파일 수정

--- TANNAE/app/src/main/java/com/example/tannae/activity/user_service/HistoryActivity.java
+++ TANNAE/app/src/main/java/com/example/tannae/activity/user_service/HistoryActivity.java
@@ -3,6 +3,7 @@ package com.example.tannae.activity.user_service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +15,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.tannae.R;
-import com.example.tannae.sub.Content;
+import com.example.tannae.network.Network;
+import com.example.tannae.sub.History;
+import com.example.tannae.sub.InnerDB;
+import com.example.tannae.sub.Toaster;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -30,25 +42,7 @@ public class HistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history);
         setViews();
         setEventListeners();
-        adapter = new ListViewAdapter();
-        adapter.addItem(new Content("history title", "내용1"));
-        adapter.addItem(new Content("제목2", "내용2"));
-        adapter.addItem(new Content("제목1", "내용1"));
-        adapter.addItem(new Content("제목2", "내용2"));
-        adapter.addItem(new Content("제목1", "내용1"));
-        adapter.addItem(new Content("제목2", "내용2"));
-        adapter.addItem(new Content("제목1", "내용1"));
-        adapter.addItem(new Content("제목2", "내용2"));
-        listView.setAdapter(adapter);
-
-    }
-
-    // < BackPress >
-    @Override
-    public void onBackPressed() {
-        Intent intent = new Intent(getApplicationContext(), UserServiceListActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
+        setAdapter();
     }
 
 
@@ -70,15 +64,50 @@ public class HistoryActivity extends AppCompatActivity {
         });
     }
 
+    private void setAdapter() {
+        try {
+            Network.service.getHistory(InnerDB.getUser().getString("usn")).enqueue(new Callback<String>() {
+                @Override
+                public void onResponse(Call<String> call, Response<String> response) {
+                    try {
+                        JSONObject res = new JSONObject(response.body());
+                        String message = res.getString("message");
+                        if (message.equals("OK")) {
+                            JSONArray lists = res.getJSONArray("result");
+                            adapter = new ListViewAdapter();
+                            for (int i = 0; i < lists.length(); i++) {
+                                JSONObject list = lists.getJSONObject(i);
+                                adapter.addItem(new History(list.getString("date"), list.getString("origin"), list.getString("dest"), list.getInt("cost")));
+                            }
+                            listView.setAdapter(adapter);
+                        } else {
+                            Toaster.show(getApplicationContext(), message);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<String> call, Throwable t) {
+                    Toaster.show(getApplicationContext(), "Error");
+                    Log.e("Error", t.getMessage());
+                }
+            });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
     public class ListViewAdapter extends BaseAdapter {
-        ArrayList<Content> items = new ArrayList<Content>();
+        ArrayList<History> items = new ArrayList<History>();
 
         @Override
         public int getCount() {
             return items.size();
         }
 
-        public void addItem(Content item) {
+        public void addItem(History item) {
             items.add(item);
         }
         @Override
@@ -94,26 +123,36 @@ public class HistoryActivity extends AppCompatActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup viewGroup) {
             final Context context = viewGroup.getContext();
-            final Content list = items.get(position);
+            final History list = items.get(position);
 
             if(convertView == null) {
                 LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                convertView = inflater.inflate(R.layout.community_listview_list_item, viewGroup, false);
+                convertView = inflater.inflate(R.layout.history_listview_list_item, viewGroup, false);
 
             }else {
                 View view = new View(context);
                 view = (View) convertView;
 
             }
+            TextView date = (TextView) convertView.findViewById(R.id.tv_date_history_listview);
+            TextView origin = (TextView) convertView.findViewById(R.id.tv_origin_history_listview);
+            TextView destination = (TextView) convertView.findViewById(R.id.tv_destination_history_listview);
+            TextView cost = (TextView) convertView.findViewById(R.id.tv_cost_history_listview);
 
-            TextView title = (TextView) convertView.findViewById(R.id.tv_title);
-            TextView content = (TextView) convertView.findViewById(R.id.tv_content);
-
-            title.setText(list.getTitle());
-            content.setText(list.getContent());
+            date.setText(list.getData("date").toString());
+            origin.setText(list.getData("origin").toString());
+            destination.setText(list.getData("destination").toString());
+            cost.setText(list.getData("cost").toString());
 
             return convertView;
         }
+    }
 
+    // < BackPress >
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(getApplicationContext(), UserServiceListActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
