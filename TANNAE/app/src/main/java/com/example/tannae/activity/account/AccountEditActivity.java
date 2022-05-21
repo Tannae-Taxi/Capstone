e333c995a9dff41441ebd06dafe267f8b4770eab
JaeWon Choi <unsyg.choi@gmail.com>
2022-05-21 15:58:39 +0900

    [Update] AccountEdit|account_edit JW05211558
    - 회원정보수정 구현
    - CODE FIX

--- TANNAE/app/src/main/java/com/example/tannae/activity/account/AccountEditActivity.java
+++ TANNAE/app/src/main/java/com/example/tannae/activity/account/AccountEditActivity.java
@@ -15,8 +15,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.tannae.R;
-import com.example.tannae.activity.user_service.UserServiceListActivity;
 import com.example.tannae.network.Network;
+import com.example.tannae.sub.InnerDB;
 import com.example.tannae.sub.Toaster;
 
 import org.json.JSONException;
@@ -26,13 +26,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+// << AccountEdit >>
 public class AccountEditActivity extends AppCompatActivity {
-    private EditText etID, etPW,etCheckPW, etEmail, etPhone;
+    private EditText etID, etPW, etCheckPW, etEmail, etPhone;
     private TextView tvCheckID, tvCheckPW;
     private Button btnCheckID, btnEdit;
     private Toolbar toolbar;
-
-    private boolean availableID = false, checkedID = false, availablePW = false, availablePWC = false, genderType = true, availableEmail = false, availablePhone = false;
+    private boolean availableID = false, checkedID = false, availablePW = false, availablePWC = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +41,8 @@ public class AccountEditActivity extends AppCompatActivity {
         setViews();
         setEventListeners();
     }
-    private void setViews(){
+
+    private void setViews() {
         etID = findViewById(R.id.et_id_account_edit);
         etPW = findViewById(R.id.et_pw_account_edit);
         etCheckPW = findViewById(R.id.et_checkpw_account_edit);
@@ -55,7 +56,8 @@ public class AccountEditActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-    private void setEventListeners(){
+
+    private void setEventListeners() {
         etID.addTextChangedListener(new TextWatcher() {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
@@ -76,43 +78,35 @@ public class AccountEditActivity extends AppCompatActivity {
                 }
             }
 
-            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-            @Override public void afterTextChanged(Editable s) { }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
         });
+
         btnCheckID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Check if ID type is available
-                if (!availableID) {
+                if (!availableID)
                     Toaster.show(getApplicationContext(), "지원되지 않는 ID 형식입니다. \n다른 ID를 사용해주세요.");
-                    //Toast.makeText(getApplicationContext(), "지원되지 않는 ID 형식입니다. \n다른 ID를 사용해주세요.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                // Request if ID is not user [RETROFIT]
-                Network.service.checkID(etID.getText().toString()).enqueue(new Callback<String>() {
-                    @Override
-                    public void onResponse(Call<String> call, Response<String> response) {
-                        String message = response.body();
-                        System.out.println(message);
-
-                        if (message.equals("OK")) {
-                            checkedID = true;
-                            Toaster.show(getApplicationContext(), "사용 가능한 ID 입니다.");
-                            //Toast.makeText(getApplicationContext(), "사용 가능한 ID 입니다.", Toast.LENGTH_SHORT).show();
-                        } else {
-                            checkedID = false;
-                            Toaster.show(getApplicationContext(), message);
-                            //Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                else
+                    Network.service.checkID(etID.getText().toString()).enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+                            String message = response.body();
+                            checkedID = message.equals("OK");
+                            Toaster.show(getApplicationContext(), message.equals("OK") ? "사용 가능한 ID 입니다." : message);
                         }
-                    }
 
-                    @Override
-                    public void onFailure(Call<String> call, Throwable t) {
-                        Toaster.show(getApplicationContext(), "Error");
-                        //Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-                        Log.e("Error", t.getMessage());
-                    }
-                });
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+                            Toaster.show(getApplicationContext(), "Error");
+                            Log.e("Error", t.getMessage());
+                        }
+                    });
             }
         });
 
@@ -136,8 +130,13 @@ public class AccountEditActivity extends AppCompatActivity {
                 }
             }
 
-            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-            @Override public void afterTextChanged(Editable s) { }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
         });
 
         // Check if PWR is identical with PW
@@ -150,7 +149,7 @@ public class AccountEditActivity extends AppCompatActivity {
                     tvCheckPW.setText("사용 불가능한 PW 형식입니다.");
                     availablePWC = false;
                 } else {
-                    if(etPW.getText().toString().equals(pwr)) {
+                    if (etPW.getText().toString().equals(pwr)) {
                         tvCheckPW.setTextColor(0xAA0000FF);
                         tvCheckPW.setText("비밀번호가 일치합니다.");
                         availablePWC = true;
@@ -162,74 +161,62 @@ public class AccountEditActivity extends AppCompatActivity {
                 }
             }
 
-            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-            @Override public void afterTextChanged(Editable s) { }
-        });
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
 
 
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /* try {
-                    // Check if entered info's are available
+                try {
                     if (!availableID || !availablePW)
                         Toaster.show(getApplicationContext(), "허용되지 않은 ID or PW 형식입니다.");
-                        //Toast.makeText(getApplicationContext(), "허용되지 않은 ID or PW 형식입니다.", Toast.LENGTH_SHORT).show();
                     else if (!checkedID)
                         Toaster.show(getApplicationContext(), "ID 중복을 확인하세요");
-                        //Toast.makeText(getApplicationContext(), "ID 중복을 확인하세요.", Toast.LENGTH_SHORT).show();
                     else if (!availablePWC)
                         Toaster.show(getApplicationContext(), "PW가 일치하지 않습니다.");
-                        //Toast.makeText(getApplicationContext(), "PW가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
                     else if (!Patterns.EMAIL_ADDRESS.matcher(etEmail.getText().toString()).matches())
                         Toaster.show(getApplicationContext(), "Email 을 정확하게 작성하세요.");
-                        //Toast.makeText(getApplicationContext(), "Email 을 정확하게 작성하세요.", Toast.LENGTH_SHORT).show();
                     else if (!Patterns.PHONE.matcher(etPhone.getText().toString()).matches())
                         Toaster.show(getApplicationContext(), "전화번호를 정확하게 작성하세요.");
-                        //Toast.makeText(getApplicationContext(), "전화번호를 정확하게 작성하세요.", Toast.LENGTH_SHORT).show();
-                        // If available request sign up [RETROFIT]
                     else {
-                        ////////////////////////////////////////////// 수정한 정보 서버DB에 보내주는 곳 채우기 Create User JSON
-                        ////////////////////////////////////////////// 수정한 정보대로 내부 DB도 수정해주어야 함
+                        JSONObject newUser = new JSONObject();
+                        newUser.put("usn", InnerDB.sp.getString("usn", null));
+                        newUser.put("id", etID.getText().toString());
+                        newUser.put("pw", etPW.getText().toString());
+                        newUser.put("email", etEmail.getText().toString());
+                        newUser.put("phone", etPhone.getText().toString());
 
-                        JSONObject reqObj = new JSONObject();
-                        reqObj.put("id", etID.getText().toString());
-                        reqObj.put("pw", etPW.getText().toString());
-                        reqObj.put("gender", genderType);
-                        reqObj.put("phone", etPhone.getText().toString());
-                        reqObj.put("email", etEmail.getText().toString());
-
-                        // Request sign up [RETROFIT]
-                        Network.service.signup(reqObj).enqueue(new Callback<Boolean>() {
+                        Network.service.editAccount(newUser).enqueue(new Callback<Boolean>() {
                             @Override
                             public void onResponse(Call<Boolean> call, Response<Boolean> response) {
-                                Toaster.show(getApplicationContext(), "회원정보 수정이 완료되었습니다.");
-
-                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                startActivity(intent);
+                                InnerDB.editor.clear().apply();
+                                Toaster.show(getApplicationContext(), "회원정보가 수정되었습니다.\n다시 로그인해주세요.");
+                                startActivity(new Intent(getApplicationContext(), LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
                             }
 
                             @Override
                             public void onFailure(Call<Boolean> call, Throwable t) {
                                 Toaster.show(getApplicationContext(), "Error");
-                                //Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
                                 Log.e("Error", t.getMessage());
                             }
                         });
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
-                } */
+                }
             }
         });
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), AccountActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
+                startActivity(new Intent(getApplicationContext(), AccountActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             }
         });
     }
