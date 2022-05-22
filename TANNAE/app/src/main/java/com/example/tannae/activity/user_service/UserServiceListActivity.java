87e75a81a3dbd68d6d4b3f9fc4566e22b336a6ee
JaeWon Choi <unsyg.choi@gmail.com>
2022-05-22 11:00:51 +0900

    [Update] UserServiceList JW05221100
    - CODE FIX

--- TANNAE/app/src/main/java/com/example/tannae/activity/user_service/UserServiceListActivity.java
+++ TANNAE/app/src/main/java/com/example/tannae/activity/user_service/UserServiceListActivity.java
@@ -2,10 +2,8 @@ package com.example.tannae.activity.user_service;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +11,6 @@ import com.example.tannae.R;
 import com.example.tannae.activity.account.AccountActivity;
 import com.example.tannae.activity.account.LoginActivity;
 import com.example.tannae.activity.main_service.MainActivity;
-import com.example.tannae.activity.main_service.PaymentActivity;
 import com.example.tannae.sub.InnerDB;
 import com.example.tannae.sub.Toaster;
 
@@ -39,79 +36,22 @@ public class UserServiceListActivity extends AppCompatActivity {
         btnLogout = findViewById(R.id.btn_logout_userservicelist);
     }
 
+    private void setEventListeners() {
+        layoutAccount.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), AccountActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
+        layoutPoint.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), PointActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
+        layoutQnA.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), QnAActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
+        layoutHistory.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), HistoryActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
+        layoutLostFound.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), LostFoundActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
+        layoutFAQ.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), FAQActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
+        btnLogout.setOnClickListener(v -> {
+            InnerDB.sp.edit().clear().apply();
+            Toaster.show(getApplicationContext(), "안전하게 로그아웃 되었습니다.");
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+        });
+    }
+    
     @Override
     public void onBackPressed() {
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
-    }
-
-    private void setEventListeners() {
-
-        layoutAccount.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), AccountActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }
-        });
-        // Service on
-
-        layoutPoint.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), PointActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }
-        });
-
-        layoutQnA.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), QnAActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }
-        });
-
-        layoutHistory.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), HistoryActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }
-        });
-
-        layoutLostFound.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), LostFoundActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }
-        });
-
-        layoutFAQ.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), FAQActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }
-        });
-
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                InnerDB.sp.edit().clear().apply(); // 내부 DB clear
-                Toaster.show(getApplicationContext(), "안전하게 로그아웃 되었습니다.");
-                //Toast.makeText(UserServiceListActivity.this, "안전하게 로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
-
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class); //LoginActivity 로 전환 //// 근데 로그아웃 기능 따로 구현 안하고이 방식으로 할 것인지 궁금
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent); // 로그아웃 기능 구현
-            }
-        });
+        startActivity(new Intent(getApplicationContext(), MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
     }
 }
