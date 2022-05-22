66fcccd7b911e2ffe12bbf59d369bfa170667411
JaeWon Choi <unsyg.choi@gmail.com>
2022-05-22 10:56:33 +0900

    [Update] QnAEdit JW05221056
    - Failure 처리 추가

--- TANNAE/app/src/main/java/com/example/tannae/activity/user_service/QnAEditActivity.java
+++ TANNAE/app/src/main/java/com/example/tannae/activity/user_service/QnAEditActivity.java
@@ -3,7 +3,6 @@ package com.example.tannae.activity.user_service;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -43,47 +42,45 @@ public class QnAEditActivity extends AppCompatActivity {
     }
 
     private void setEventListeners() {
-        btnEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    if (getIntent().getBooleanExtra("flag", true))
-                        Network.service.postContent(InnerDB.getUser()
-                                .put("title", etTitle.getText().toString())
-                                .put("content", etContent.getText().toString()))
-                                .enqueue(new Callback<Boolean>() {
-                                    @Override
-                                    public void onResponse(Call<Boolean> call, Response<Boolean> response) {
-                                        Toaster.show(getApplicationContext(), "QnA가 등록되었습니다.");
-                                        startActivity(new Intent(getApplicationContext(), QnAActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                                    }
+        btnEdit.setOnClickListener(v -> {
+            try {
+                if (getIntent().getBooleanExtra("flag", true))
+                    Network.service.postContent(InnerDB.getUser()
+                            .put("title", etTitle.getText().toString())
+                            .put("content", etContent.getText().toString()))
+                            .enqueue(new Callback<Boolean>() {
+                                @Override
+                                public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                                    Toaster.show(getApplicationContext(), "QnA가 등록되었습니다.");
+                                    startActivity(new Intent(getApplicationContext(), QnAActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                                }
 
-                                    @Override
-                                    public void onFailure(Call<Boolean> call, Throwable t) {
+                                @Override
+                                public void onFailure(Call<Boolean> call, Throwable t) {
+                                    Toaster.show(getApplicationContext(), "Error");
+                                    Log.e("Error", t.getMessage());
+                                }
+                            });
+                else
+                    Network.service.editContent(InnerDB.getUser()
+                            .put("csn", getIntent().getStringExtra("csn"))
+                            .put("title", etTitle.getText().toString())
+                            .put("content", etContent.getText().toString()))
+                            .enqueue(new Callback<Boolean>() {
+                                @Override
+                                public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                                    Toaster.show(getApplicationContext(), "QnA가 수정되었습니다.");
+                                    startActivity(new Intent(getApplicationContext(), QnAActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                                }
 
-                                    }
-                                });
-                    else
-                        Network.service.editContent(InnerDB.getUser()
-                                .put("csn", getIntent().getStringExtra("csn"))
-                                .put("title", etTitle.getText().toString())
-                                .put("content", etContent.getText().toString()))
-                                .enqueue(new Callback<Boolean>() {
-                                    @Override
-                                    public void onResponse(Call<Boolean> call, Response<Boolean> response) {
-                                        Toaster.show(getApplicationContext(), "QnA가 수정되었습니다.");
-                                        startActivity(new Intent(getApplicationContext(), QnAActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                                    }
-
-                                    @Override
-                                    public void onFailure(Call<Boolean> call, Throwable t) {
-                                        Toaster.show(getApplicationContext(), "Error");
-                                        Log.e("Error", t.getMessage());
-                                    }
-                                });
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                                @Override
+                                public void onFailure(Call<Boolean> call, Throwable t) {
+                                    Toaster.show(getApplicationContext(), "Error");
+                                    Log.e("Error", t.getMessage());
+                                }
+                            });
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
         });
         toolbar.setNavigationOnClickListener(v -> onBackPressed());
