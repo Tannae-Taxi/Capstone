f5399c081032d822095ea33bd42c8b8e6093b92a
JaeWon Choi <unsyg.choi@gmail.com>
2022-05-23 20:36:08 +0900

    [Update] UserServiceList JW05232036
    - CODE REVIEW

--- TANNAE/app/src/main/java/com/example/tannae/activity/user_service/UserServiceListActivity.java
+++ TANNAE/app/src/main/java/com/example/tannae/activity/user_service/UserServiceListActivity.java
@@ -2,7 +2,6 @@ package com.example.tannae.activity.user_service;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,10 +21,10 @@ public class UserServiceListActivity extends AppCompatActivity {
     }
 
     private void setViews() {
-        ((LinearLayout) findViewById(R.id.layout_account_userservicelist)).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), AccountActivity.class)));
-        ((LinearLayout) findViewById(R.id.layout_point_userservicelist)).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), PointActivity.class)));
-        ((LinearLayout) findViewById(R.id.layout_qna_userservicelist)).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), QnAActivity.class)));
-        ((LinearLayout) findViewById(R.id.layout_history_userservicelist)).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), HistoryActivity.class)));
+        findViewById(R.id.layout_account_userservicelist).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), AccountActivity.class)));
+        findViewById(R.id.layout_point_userservicelist).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), PointActivity.class)));
+        findViewById(R.id.layout_qna_userservicelist).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), QnAActivity.class)));
+        findViewById(R.id.layout_history_userservicelist).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), HistoryActivity.class)));
         (findViewById(R.id.layout_lost_found_userservicelist)).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), LostFoundActivity.class)));
         (findViewById(R.id.layout_faq_userservicelist)).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), FAQActivity.class)));
         (findViewById(R.id.btn_logout_userservicelist)).setOnClickListener(v -> {
